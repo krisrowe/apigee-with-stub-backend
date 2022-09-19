29ec0919f19f17093d3cb2f9b2a6d9a6cc270da7
Kris Rowe <krowe@rowelink.com>
2022-09-19 00:33:47 +0000

    VM creation working

--- provision.sh
+++ provision.sh
@@ -7,14 +7,15 @@ fi
 export RUNTIME_LOCATION=us-central1
 export ANALYTICS_LOCATION=us-central1
 
-export user=$(gcloud config get-value account)
-exit
-if [ -z "${user}" ]; then
+export GACCT=$(gcloud config get-value account)
+echo $GACCT
+
+if [ -z "${GACCT}" ]; then
   gcloud auth login
 fi
 
 gcloud config set project $1
-export PROJECT_NUMBER=$(gcloud projects describe $PROJECT_ID --format="value(projectNumber)")
+export PROJECT_NUMBER=$(gcloud projects describe $1 --format="value(projectNumber)")
 
 retVal=$?
 if [ $retVal -ne 0 ]; then
@@ -23,12 +24,15 @@ else
    echo "Project set"
 fi
 
-sudo docker pull gcr.io/apijamkr/stubbed-service
+#sudo docker pull gcr.io/apijamkr/stubbed-service
 
-gcloud compute instances create-with-container stubvm --network $1 \
-    --container-image sudo docker pull gcr.io/apijamkr/stubbed-service
+# The --no-address prevents an external IP from being assigned. Not only
+# is this the intention, but it's also required by some organizations' policies.
+# The --shielded-secure-boot is also required by some organizations' policies.
+gcloud compute instances create-with-container stubvm --network=$2 --zone=us-central1-a \
+  --container-image=gcr.io/apijamkr/stubbed-service --subnet=default --no-address --shielded-secure-boot
 
-export STATIC_IP=$(gcloud compute addresses describe stubvm --region $RUNTIME_LOCATION --format='value(address)')
+export STATIC_IP=$(gcloud compute instances describe stubvm --zone=us-central1-a --format='get(networkInterfaces[0].networkIP)')
 echo $STATIC_IP
 
 
